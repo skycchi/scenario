24240099f926d961637d5fa84af1054a379575a6
Adilene Ricardo <adilene@Adilenes-MacBook-Air.local>
2023-06-06 19:47:28 -0700

    barba.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,9 @@ document.onclick = function() {
 }
 click.volume = 0.5;
 
+var audio = document.getElementById("go");
+audio.volume = 0.5;
+
 function shuffle(){
     var hints = $('.box');
     var i = 0;
@@ -15,24 +18,43 @@ function shuffle(){
     })
 }
 
+function animate(){
+    function animateValue(obj, start, end, duration) {
+        let startTimestamp = null;
+        const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        obj.innerHTML = Math.floor(progress * (end - start) + start);
+        if (progress < 1) {
+          window.requestAnimationFrame(step);
+        }
+        };
+        window.requestAnimationFrame(step);
+        
+    }
+    
+    const obj = document.getElementById("value");
+    animateValue(obj, 100, 0, 5000);
+
+    $("#subtitle").delay(5000).fadeIn();
+
+    document.onclick = function() {
+        $("#subtitle").fadeOut(50);
+        $("#options").fadeIn(100);
+    }
+}
+
 $(document).ready(function() {
     shuffle();
+    animate();
 });
 
 barba.hooks.after(() => {
     shuffle();
+    animate();
 });                    
 
 barba.init({
-    views: [{
-        before({ next }) {
-
-        // load your script
-        let script = document.createElement('script');
-        script.src = '/scripts/shuffle.js'; // location of your draggable js file that is responsible for that image loading and dragging functionality
-        next.container.appendChild(script);
-        }, 
-    }],
   transitions: [{
     name: 'opacity-transition',
     leave(data) {
